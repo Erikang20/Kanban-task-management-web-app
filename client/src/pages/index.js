eb58019867cd99d7c9c5502688d295e9557473d2
MONIKA SINGH <92215187+Monika5S@users.noreply.github.com>
2024-06-28 23:46:45 +0530

    added kanban page to show app layout

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,14 +1,11 @@
 import { SEO } from "@src/components/SEO";
 import HomepageContent from "@components/Home";
-import AppLayout from "@src/layout";
 
 export default function Home() {
 	return (
 		<>
 			<SEO title={"Homepage"} description={"Kanban task app"} />
-			{/* <HomepageContent /> */}
-
-			<AppLayout />
+			<HomepageContent />
 		</>
 	);
 }
