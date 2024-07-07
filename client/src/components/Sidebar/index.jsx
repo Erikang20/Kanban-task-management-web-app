690a05c0fd58aa8e87a6d77770f8a04545e9c332
Jessica Vargas <139159958+javar10@users.noreply.github.com>
2024-07-07 15:38:23 -0400

    basic functionality and structure of add new board modal

--- client/src/components/Sidebar/index.jsx
+++ client/src/components/Sidebar/index.jsx
@@ -7,10 +7,12 @@ import darkThemeIcon from "@assets/icon-dark-theme.svg";
 import lightThemeIcon from "@assets/icon-light-theme.svg";
 import logoLight from "@assets/logo-light.svg";
 import Slider from "@components/Sidebar/slider";
+import AddNewBoard from "@components/core/AddNewBoard/AddNewBoard";
 
 const Sidebar = () => {
 	const [isHidden, setIsHidden] = useState(false);
 	const [isToggled, setIsToggle] = useState(false);
+	const [addNewBoardModalOpen, setAddNewBoardModalOpen] = useState(false);
 
 	const toggleSidebar = () => {
 		setIsHidden(!isHidden);
@@ -40,10 +42,23 @@ const Sidebar = () => {
 					</div>
 					<span className={styles.allBoards}>ALL BOARDS</span>
 					<div className={styles.linkSection}>
-						<a href="#">Platform Launch</a>
-						<a href="#">Marketing Plan</a>
-						<a href="#">Roadmap</a>
+						<a href="#" className={styles.sidebarLink}>Platform Launch</a>
+						<a href="#" className={styles.sidebarLink}>Marketing Plan</a>
+						<a href="#" className={styles.sidebarLink}>Roadmap</a>
+						<a href="#" className={`${styles.createNewBoardLink} ${styles.sidebarLink}`}
+							onClick={(e) => {
+								e.preventDefault();
+								setAddNewBoardModalOpen(!addNewBoardModalOpen);
+							}}>+Create New Board</a>
+
+						{addNewBoardModalOpen &&
+							<AddNewBoard
+								addNewBoardModalOpen={addNewBoardModalOpen}
+								setAddNewBoardModalOpen={setAddNewBoardModalOpen}
+							/>
+						}
 					</div>
+
 					<div className={styles.lightDarkModeIconSection}>
 						<Image
 							className={styles.lightModeIcon}
