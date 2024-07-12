b9e4f23c937313f2f55a79ae9943e12fc9249981
Kaleigh Scruggs <kaleighscruggs@gmail.com>
2024-07-12 16:10:17 -0400

    add dropdown

--- client/src/components/Header/ThreeDotsMenu.jsx
+++ client/src/components/Header/ThreeDotsMenu.jsx
@@ -1,15 +1,52 @@
-import Image from "next/image";
 import verticalEllipsis from "@assets/icon-vertical-ellipsis.svg";
+import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
 import styles from "./styles.module.scss";
 
 export const ThreeDotsMenu = () => {
+	const [menuVisible, setMenuVisible] = useState(false);
+	const menuRef = useRef(null);
+
+	const toggleMenu = () => {
+		setMenuVisible(!menuVisible);
+	};
+
+	const handleClickOutside = (event) => {
+		if (menuRef.current && !menuRef.current.contains(event.target)) {
+			setMenuVisible(false);
+		}
+	};
+
+	useEffect(() => {
+		if (menuVisible) {
+			document.addEventListener("mousedown", handleClickOutside);
+		} else {
+			document.removeEventListener("mousedown", handleClickOutside);
+		}
+
+		return () => {
+			document.removeEventListener("mousedown", handleClickOutside);
+		};
+	}, [menuVisible]);
+
 	return (
-		<Image
-			className={styles.verticalEllipsis}
-			src={verticalEllipsis}
-			width="50"
-			height="15"
-			alt="icon"
-		/>
+		<div className={styles.container} ref={menuRef}>
+			<Image
+				className={styles.verticalEllipsis}
+				src={verticalEllipsis}
+				width={50}
+				height={15}
+				alt="icon"
+				onClick={toggleMenu}
+			/>
+			{menuVisible && (
+				<div className={styles.menu}>
+					<ul>
+						<li className={styles.edit}>Edit Board</li>
+						<li className={styles.delete}>Delete Board</li>
+					</ul>
+				</div>
+			)}
+		</div>
 	);
 };
