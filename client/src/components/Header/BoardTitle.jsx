00f55c2e1f0782828f0823d037b55661b1a8b449
Jessica Vargas <139159958+javar10@users.noreply.github.com>
2024-07-20 16:42:24 -0400

    heading stg styling

---
+++ client/src/components/Header/BoardTitle.jsx
@@ -0,0 +1,9 @@
+import styles from './styles.module.scss'
+
+const BoardTitle = () => {
+  return (
+    <div className={styles.boardTitle}>Platform Launch</div>
+  )
+}
+
+export default BoardTitle
--- client/src/components/Header/header.jsx
+++ client/src/components/Header/header.jsx
@@ -3,6 +3,7 @@ import AddNewTaskBtn from "@components/Header/addTaskBtn";
 import LogoHeader from "@components/Header/logo";
 import styles from "./styles.module.scss";
 import { ThreeDotsMenu } from "./ThreeDotsMenu";
+import BoardTitle from "./BoardTitle";
 
 
 export const Header = () => {
@@ -10,10 +11,20 @@ export const Header = () => {
 
 	return (
 		<div className={styles.headerRoot}>
-			<LogoHeader theme={theme} />
-			<div className={styles.headerButtons}>
-				<AddNewTaskBtn />
-				<ThreeDotsMenu />
+			<div className={styles.headerLogoDiv}>
+				<LogoHeader theme={theme} />
+			</div>
+			<div className={styles.headerTextDiv}>
+				<div>
+					<BoardTitle />
+				</div>
+
+				<div className={styles.headerButtons}>
+					<AddNewTaskBtn />
+					<ThreeDotsMenu />
+				</div>
+
+
 			</div>
 
 		</div>
