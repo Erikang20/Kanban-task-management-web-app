12453da0422d93ee37178cf558348144c07a527c
Jessica Vargas <139159958+javar10@users.noreply.github.com>
2024-07-23 14:41:45 -0400

    removed duplicate files

--- client/src/components/Header/BoardTitle 2.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './styles.module.scss'
-
-const BoardTitle = () => {
-  return (
-    <div className={styles.boardTitle}>Platform Launch</div>
-  )
-}
-
-export default BoardTitle
