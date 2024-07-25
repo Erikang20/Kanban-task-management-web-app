cba7bcda8041b60819e6d7c5876ec427d15d5bd7
Jessica Vargas <139159958+javar10@users.noreply.github.com>
2024-07-24 20:32:45 -0400

    webpack config for svg

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,13 +1,21 @@
-import path from "path";
+import path from 'path';
 
-const __dirname = path.dirname("./src/styles/*");
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	sassOptions: {
-		includePaths: [path.join(__dirname, "styles")],
-	},
+  reactStrictMode: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
+
